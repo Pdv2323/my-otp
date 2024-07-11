5c42fff001df1214fa0bd04c497c86c3eea23e53
Pdv2323 <Parthvinchhi21112000@gmail.com>
2024-07-11 18:14:03 +0530

    Re-initialized mod file

--- cmd/main.go
+++ cmd/main.go
@@ -2,37 +2,35 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"time"
 
-	"github.com/Pdv2323/otp"
+	"github.com/Pdv2323/my-otp/email"
+	"github.com/Pdv2323/my-otp/generate"
+	"github.com/Pdv2323/my-otp/verify"
 )
 
-// func main() {
-// 	var userEmail string
-// 	fmt.Print("Enter your email: ")
-// 	fmt.Scanln(&userEmail)
-
-// 	otp := generate.GenerateOtp()
-
-// 	err := email.SendEmail(userEmail, otp)
-// 	if err != nil {
-// 		log.Fatalf("Error sending email to %s.", userEmail)
-// 	}
-
-// 	fmt.Println("OTP sent successfully! Check your email.")
-
-// 	time.Sleep(1000)
-
-// 	var NewOtp int
-// 	fmt.Print("Enter the Otp you received : ")
-// 	fmt.Scan(&NewOtp)
-
-// 	v := verify.VerifyOtp(otp, NewOtp)
-// 	fmt.Println(v)
-
-// }
-
 func main() {
-	digit := otp.GenerateOtp(3)
+	var userEmail string
+	fmt.Print("Enter your email: ")
+	fmt.Scanln(&userEmail)
 
-	fmt.Print(digit)
+	otp := generate.GenerateOtp()
+
+	err := email.SendEmail(userEmail, otp)
+	if err != nil {
+		log.Fatalf("Error sending email to %s.", userEmail)
+	}
+
+	fmt.Println("OTP sent successfully! Check your email.")
+
+	time.Sleep(1000)
+
+	var NewOtp int
+	fmt.Print("Enter the Otp you received : ")
+	fmt.Scan(&NewOtp)
+
+	v := verify.VerifyOtp(otp, NewOtp)
+	fmt.Println(v)
+
 }
