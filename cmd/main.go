3bfc0372ccf39e6f830b84dd6fefeebc94dd43bc
parthvinchhi <parthvinchhi21112000@gmail.com>
2024-07-31 13:11:23 +0530

    Cleaned code

--- cmd/main.go
+++ cmd/main.go
@@ -7,53 +7,13 @@ import (
 	"github.com/parthvinchhi/my-otp/pkg/handlers"
 )
 
-// func main() {
-// 	var userEmail string
-// 	fmt.Print("Enter your email: ")
-// 	fmt.Scanln(&userEmail)
-
-// 	otp := handlers.GenerateOtpHandler(6)
-
-// 	err := handlers.SendEmailHandler(userEmail, otp)
-// 	if err != nil {
-// 		log.Fatalf("Error sending email to %s.", userEmail)
-// 	}
-
-// 	fmt.Println("OTP sent successfully! Check your email.")
-
-// 	time.Sleep(1000)
-
-// 	var NewOtp int
-// 	fmt.Print("Enter the Otp you received : ")
-// 	fmt.Scan(&NewOtp)
-
-// 	v := handlers.VerifyOtpHandler(otp, NewOtp)
-// 	fmt.Println(v)
-
-// }
-
-// func main() {
-// 	mux := http.NewServeMux()
-// 	mux.HandleFunc("/", handlers.RenderIndexPageHandler)
-// 	mux.HandleFunc("/submit-email", handlers.SubmitEmailIdHandler)
-// 	mux.HandleFunc("/submit-otp", handlers.SubmitOTPHandler)
-
-// 	fmt.Println("Starting server on :8888")
-// 	http.ListenAndServe(":8888", mux)
-// }
-
 func main() {
 	r := gin.Default()
 
 	r.LoadHTMLGlob("pkg/templates/*")
 
-	// indexFile := "pkg/templates/index.html"
-	// verifyFile := "pkg/templates/verify.html"
-
 	indexFile := "index.html"
 	verifyFile := "verify.html"
-
-	// r.LoadHTMLFiles(indexFile, verifyFile)
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, indexFile, nil)
---
+++ pkg/.handlers/handlers.go
@@ -0,0 +1,217 @@
+package handlers
+
+import (
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"log"
+	"math/rand"
+	"net/http"
+	"net/smtp"
+	"reflect"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+// type Otp struct {
+// 	Otp string
+// }
+
+var Otp int
+
+func GenerateOtpHandler(n int) int {
+	if n < 1 {
+		return 0
+		// return string(0)
+	}
+
+	min := 1
+	for i := 1; i < n; i++ {
+		min *= 10
+	}
+	max := min*10 - 1
+	return rand.Intn(max-min+1) + min
+
+	// rand.Seed(time.Now().UnixNano())
+	// t := time.Now().UnixNano()
+	// rand.Seed(uint64(t))
+	// r := rand.Intn(max-min+1) + min
+	// t1 := reflect.TypeOf(r)
+	// log.Println(r, t1)
+	// return r
+	// return string(r)
+}
+
+func SendEmailHandler(to string, otp int) error {
+	from := "parthdv2111@gmail.com"
+	password := "dwvntkfkeqnejzco"
+	SmtpServer := "smtp.gmail.com"
+	SmtpPort := "587"
+
+	msg := fmt.Sprintf("Subject : OTP Verification\n\n Your OTP is : %d", otp)
+
+	auth := smtp.PlainAuth("", from, password, SmtpServer)
+
+	err := smtp.SendMail(
+		fmt.Sprintf("%s:%s", SmtpServer, SmtpPort),
+		auth,
+		from,
+		[]string{to},
+		[]byte(msg),
+	)
+
+	return err
+}
+
+func VerifyOtpHandler(otp, newOtp string) bool {
+	// fmt.Scanln("Enter the Otp you received : ", &otp)
+	if otp != newOtp {
+		return false
+	}
+	return true
+}
+
+func RenderIndexPageHandler(w http.ResponseWriter, r *http.Request) {
+	// t, err := template.ParseFiles("pkg/templates/index.html")
+	// if err != nil {
+	// 	fmt.Fprintf(w, "Error: %s", err)
+	// 	return
+	// }
+	// t.Execute(w, nil)
+	http.ServeFile(w, r, "pkg/templates/index.html")
+}
+
+func RenderVerifyPageHandler(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("pkg/templates/verify.html")
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err)
+	}
+	t.Execute(w, nil)
+}
+
+func SubmitEmailIdHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		Email := r.FormValue("email")
+		Otp = GenerateOtpHandler(6)
+		t1 := reflect.TypeOf(Otp)
+		t2 := reflect.TypeOf(Email)
+
+		log.Println(Otp, t1)
+		err := SendEmailHandler(Email, Otp)
+		if err != nil {
+			Response := map[string]string{
+				"status":  "error",
+				"message": fmt.Sprintf("Failed to send OTP: %v", err),
+			}
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			json.NewEncoder(w).Encode(Response)
+			return
+		}
+		http.Redirect(w, r, "/submit-otp?email="+Email, http.StatusSeeOther)
+		log.Println(Otp, t1)
+		log.Println(Email, t2)
+	} else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func SubmitOTPHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		OTP := r.FormValue("otp")
+		// ok := VerifyOtpHandler(Otp, OTP)
+		// log.Println(ok)
+		// if ok {
+		// 	Response := map[string]string{
+		// 		"status":  "success",
+		// 		"message": "OTP verified successfully.",
+		// 	}
+		// 	w.WriteHeader(http.StatusOK)
+		// 	w.Header().Set("Content-Type", "application/json")
+		// 	json.NewEncoder(w).Encode(Response)
+
+		// } else {
+		// 	Response := map[string]string{
+		// 		"status":  "error",
+		// 		"message": "Invalid OTP",
+		// 	}
+		// 	w.WriteHeader(http.StatusUnauthorized)
+		// 	w.Header().Set("Content-Type", "application/json")
+		// 	json.NewEncoder(w).Encode(Response)
+		// }
+
+		IntOtp, err := strconv.Atoi(OTP)
+		if err != nil {
+			log.Fatal("Error converting string to int")
+		}
+
+		if IntOtp != Otp {
+			t1 := reflect.TypeOf(OTP)
+			t2 := reflect.TypeOf(Otp)
+
+			log.Println("ottttppps:::::", OTP, t1, Otp, t2)
+			Response := map[string]string{
+				"status":  "error",
+				"message": "Invalid OTP",
+			}
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(Response)
+		} else {
+			Response := map[string]string{
+				"status":  "success",
+				"message": "OTP verified successfully.",
+			}
+			w.WriteHeader(http.StatusOK)
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(Response)
+
+		}
+	} else if r.Method == "GET" {
+		http.ServeFile(w, r, "pkg/templates/verify.html")
+	} else {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func SubmitEmailHandler(c *gin.Context) {
+	email := c.PostForm("email")
+	Otp = GenerateOtpHandler(6)
+	err := SendEmailHandler(email, Otp)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "error",
+			"message": fmt.Sprintf("Failed to send OTP: %v", err),
+		})
+		return
+	}
+
+	c.Redirect(http.StatusSeeOther, "/submit-otp?email="+email)
+}
+
+func SubmitOtpHandler(c *gin.Context) {
+	otp := c.PostForm("otp")
+
+	IntOtp, err := strconv.Atoi(otp)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "error",
+			"message": "Failed to convert String otp to Int otp.",
+		})
+	}
+
+	if IntOtp == Otp {
+		c.JSON(http.StatusOK, gin.H{
+			"status":  "success",
+			"message": "OTP verified Successfully.",
+		})
+	} else {
+		c.JSON(http.StatusNotAcceptable, gin.H{
+			"status":  "error",
+			"message": "Wrong OTP. Please enter correct OTP.",
+		})
+
+		return
+	}
+}
